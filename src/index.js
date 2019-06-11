3d73a1b8aa534ae698e77cebfaec8e7dbef9d5fb
Adel <htwr86@gmail.com>
2019-06-11 22:07:32 +0300

    before lesson 25

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder='search' type="text"/>
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+      <li></li>
+    </ul>
+  </div>
+)
+
+ReactDom.render(el, document.getElementById('root'));
