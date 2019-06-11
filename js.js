609d651cbbdc2ddf16119a80783da052d279c4dc
Adel <htwr86@gmail.com>
2019-06-11 20:54:20 +0300

    initial commit

---
+++ js.js
@@ -0,0 +1,13 @@
+// max(1, 3);
+// max(1, 3, 3, 4, 5);
+
+// function max(... numbers) {
+//   console.log(numbers);
+// }
+
+// max(1, 3, 4);
+
+const arr = [1, 2, 3];
+
+const res = Math.max(... arr);
+console.log(res);
