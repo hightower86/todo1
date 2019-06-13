f71b6e31a3712d119b66d427ca71ffcb3837b47a
Adel <htwr86@gmail.com>
2019-06-13 18:59:27 +0300

    39 done. own event system

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,8 +21,11 @@ const App = () => {
         <SearchPanel />
         <ItemStatusFilter />
       </div>
-      
-      <TodoList todos={todoData} />
+
+      <TodoList 
+        todos={todoData}
+        onDeleted={(id) => console.log('del ', id)}
+      />
     </div>
   );
 };
