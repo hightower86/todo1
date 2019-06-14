939a8f0f6207b7ae943783c372390b80f7b26141
Adel <htwr86@gmail.com>
2019-06-14 19:40:47 +0300

    44 done. form

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 
 import './ItemAddForm.css';
 
-export default ({adding}) => {
-  return (
-    <div className='d-flex mt-3'>
-      <input className='form-control add-input' type="text" placeholder='type the task'/>
-      <button type="button"
-              onClick={()=>adding('hello world')}
+
+export default class ItemAddForm extends Component {
+  state = {
+    label: ''
+  }
+
+  searchOnChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.adding(this.state.label)
+  }
+
+  render() {
+    return (
+      <form className='d-flex mt-3'
+        onSubmit={this.onSubmit}>
+        <input className='form-control add-input' 
+                type="text" 
+                placeholder='type the task'
+                onChange={this.searchOnChange}
+        />
+        <button type="button"
                 className="btn btn-sm btn-outline-secondary">Add</button>
-    </div>
-  );
-};
+      </form>
+    )
+  }
+}
