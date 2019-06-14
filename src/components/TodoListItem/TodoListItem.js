04becea276d112ad8dc5295bc3ba2892da85ab6f
Adel <htwr86@gmail.com>
2019-06-14 11:51:02 +0300

    42 done. data refactor

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -2,70 +2,45 @@ import React, { Component } from "react";
 
 import './TodoListItem.css';
 
-export default class TodoListItem extends Component {
-
-  state = {
-    done: false,
-    important: false,
-  };
-
-  onLabelClick = () => {
-    this.setState(({done}) => {
-      return {
-        done: !done
-      };
-    });
-  }
-
-  onMarkImportant = () => {
-    this.setState(({important}) => {
-      return {
-        important: !important
-      };
-    });
-  };
-
-  render() {
-    const { label, onDeleted,
+export default (props) => {
+  const { label, onDeleted,
             onToggleImportant, 
-            onToggleDone } = this.props;
-    const { done, important } = this.state;
+            onToggleDone,
+            done, important } = props;
 
-    let classNames = 'todo-list-item';
-    if (done) {
-      classNames += ' done';
-    }
-
-    if (important) {
-      classNames += ' important';
-    }
-
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
-
-    return (
-      <span className={classNames}>
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={onToggleDone}>
-          {label}
-        </span>
-  
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right"
-                onClick={onToggleImportant}>
-          <i className="fa fa-exclamation" />
-        </button>
-  
-        <button type="button"
-                onClick={onDeleted}
-                className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa fa-trash-o" />
-        </button>
-      </span>
-    );
+  let classNames = 'todo-list-item';
+  if (done) {
+    classNames += ' done';
   }
+
+  if (important) {
+    classNames += ' important';
+  }
+
+  const style = {
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal'
+  };
+  return (
+    <span className={classNames}>
+      <span
+        className="todo-list-item-label"
+        style={style}
+        onClick={onToggleDone}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={onToggleImportant}>
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              onClick={onDeleted}
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
 }
